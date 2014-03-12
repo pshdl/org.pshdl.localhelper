a56901d5e747b9fe9c0cb1a2a61d87aee1380236
Karsten Becker <KB@Karsten-Becker.de>
2014-03-12 17:25:16 +0100

    Improved building of apps

--- buildApps.sh
+++ buildApps.sh
@@ -6,37 +6,72 @@ ROT=`tput setaf 1`;
 GRUEN=`tput setaf 2`;
 NORMAL=`tput sgr0`;
 
+cd $PSHDL/org.pshdl.localhelper
 
-echo -n "Building macosx64 package: "
-if mvn -P macosx64 package; then
+for platform in linux32 linux64 macosx64 win32 win64; do
+	echo -n "Building $platform package: "
+	if mvn -Dpshdl.root=/Users/karstenbecker/Dropbox/PSHDL -P $platform clean package; then
+		echo "${GRUEN}done${NORMAL}";
+		cp $PSHDL/org.pshdl.localhelper/target/$platform/localhelper-*.jar $PSHDL/wiki.localhelper/localhelper-$platform.jar
+	else
+		echo "${ROT}failed${NORMAL}";
+		exit 1;
+	fi
+done
+
+cd $PSHDL/org.pshdl.localhelper
+echo -n "Code signing fpga_programmer with JRE: "
+if codesign -s "Karsten Becker" target/macosx64/PSHDLLocalHelperJRE.app/Contents/MacOS/fpga_programmer; then
 	echo "${GRUEN}done${NORMAL}";
 else
 	echo "${ROT}failed${NORMAL}";
 	exit 1;
 fi
-echo -n "Code signing fpga_programmer "
-if codesign -s "Karsten Becker" target/PSHDLLocalHelper.app/Contents/MacOS/fpga_programmer; then
+
+echo -n "Code signing JRE: "
+if codesign -s "Karsten Becker" target/macosx64/PSHDLLocalHelperJRE.app/Contents/PlugIns/jdk*.jdk; then
 	echo "${GRUEN}done${NORMAL}";
 else
 	echo "${ROT}failed${NORMAL}";
 	exit 1;
 fi
 
-echo -n "Code signing Mac OS App "
-if codesign -s "Karsten Becker" target/PSHDLLocalHelper.app; then
+echo -n "Code signing Mac OS App with JRE: "
+if codesign -s "Karsten Becker" target/macosx64/PSHDLLocalHelperJRE.app; then
 	echo "${GRUEN}done${NORMAL}";
 else
 	echo "${ROT}failed${NORMAL}";
 	exit 1;
 fi
 echo "Zipping up and moving to wiki"
-cd target
+cd target/macosx64
+tar -cjf PSHDLLocalHelperMacJRE.app.tar.bz2 PSHDLLocalHelperJRE.app
+mv PSHDLLocalHelperMacJRE.app.tar.bz2 $PSHDL/wiki.localhelper/PSHDLLocalHelperMacJRE.tar.bz2
+
+cd $PSHDL/org.pshdl.localhelper
+echo -n "Code signing fpga_programmer: "
+if codesign -s "Karsten Becker" target/macosx64/PSHDLLocalHelper.app/Contents/MacOS/fpga_programmer; then
+	echo "${GRUEN}done${NORMAL}";
+else
+	echo "${ROT}failed${NORMAL}";
+	exit 1;
+fi
+
+echo -n "Code signing Mac OS App: "
+if codesign -s "Karsten Becker" target/macosx64/PSHDLLocalHelper.app; then
+	echo "${GRUEN}done${NORMAL}";
+else
+	echo "${ROT}failed${NORMAL}";
+	exit 1;
+fi
+echo "Zipping up and moving to wiki"
+cd target/macosx64
 tar -cjf PSHDLLocalHelperMacNoJRE.app.tar.bz2 PSHDLLocalHelper.app
 mv PSHDLLocalHelperMacNoJRE.app.tar.bz2 $PSHDL/wiki.localhelper/PSHDLLocalHelperMacNoJRE.tar.bz2
 
 cd $PSHDL/wiki.localhelper
 echo -n "Adding app to git commit:   "
-if git add PSHDLLocalHelperMacNoJRE.tar.bz2; then
+if git add *.tar.bz2 *.jar; then
     echo "${GRUEN}done${NORMAL}";
 else
     echo "${ROT}failed${NORMAL}";
